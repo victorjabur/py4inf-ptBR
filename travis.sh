fd5fc260fa867d0a2d97bdc0bcdb3acff1db0a3c
Elio Capelati Jr <elio@capelati.com>
2015-12-13 19:07:47 -0200

    book info

--- travis.sh
+++ travis.sh
@@ -10,6 +10,7 @@ latex book
 makeindex book
 latex book
 dvipdf book.dvi book.pdf
+ls -larth book.pdf
 
 echo Removed temporary files
 rm -f book.aux book.ind book.ilg book.log book.dvi book.idx book.toc book.haux book.hind book.image.tex book.tmp book.idv book.4tc book.lg book.xref
