a4891bcb3f843692127ec0346998cd47e7629b69
Elio Capelati Jr <elio@capelati.com>
2015-12-13 19:48:29 -0200

    deploy s3, fix region us-east-1, and enabling multiples pdf names

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-startAt=$(date)
+startAt=$(date +%Y%m%d_%H%M%S_%s)
 
 echo "Generating book"
 
@@ -11,8 +11,8 @@ mkdir -p $HOME/.py4inf/pdf
 latex book
 makeindex book
 latex book
-dvipdf book.dvi $HOME/.py4inf/pdf/book.pdf
-ls -larth $HOME/.py4inf/pdf/book.pdf
+dvipdf book.dvi $HOME/.py4inf/pdf/book$startAt.pdf
+ls -larth $HOME/.py4inf/pdf/book_$startAt.pdf
 
 echo Removed temporary files
 rm -f book.aux book.ind book.ilg book.log book.dvi book.idx book.toc book.haux book.hind book.image.tex book.tmp book.idv book.4tc book.lg book.xref
