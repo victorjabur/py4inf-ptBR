92e9bb70abb26131326f695c43851f4a3eae37de
Elio Capelati Jr <elio@capelati.com>
2015-12-13 17:35:55 -0200

    travis cache

--- travis.sh
+++ travis.sh
@@ -10,6 +10,10 @@ makeindex book
 latex book
 dvipdf book.dvi book.pdf
 
+mkdir -p $HOME/.py4inf/pdf
+
+cp book.pdf $HOME/.py4inf/pdf/book$(date +%Y%m%d_%H%M%S_%s).pdf
+
 echo Removed temporary files
 rm -f book.aux book.ind book.ilg book.log book.dvi book.idx book.toc book.haux book.hind book.image.tex book.tmp book.idv book.4tc book.lg book.xref
 
