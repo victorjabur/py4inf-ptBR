5f51605a31060cec92c544c4cb8754f457ddd21e
Elio Capelati Jr <elio@capelati.com>
2015-12-13 19:41:28 -0200

    artifacts>deploy s3

--- travis.sh
+++ travis.sh
@@ -6,11 +6,13 @@ startAt=$(date)
 
 echo "Generating book"
 
+
+mkdir -p $HOME/.py4inf/pdf
 latex book
 makeindex book
 latex book
-dvipdf book.dvi book.pdf
-ls -larth book.pdf
+dvipdf book.dvi $HOME/.py4inf/pdf/book.pdf
+ls -larth $HOME/.py4inf/pdf/book.pdf
 
 echo Removed temporary files
 rm -f book.aux book.ind book.ilg book.log book.dvi book.idx book.toc book.haux book.hind book.image.tex book.tmp book.idv book.4tc book.lg book.xref
