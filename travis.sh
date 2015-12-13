5cc2e2b0252e99f67c4897dd5b0f87cc037b2f48
Elio Capelati Jr <elio@capelati.com>
2015-12-13 16:24:20 -0200

    support for travis ci build, script

---
+++ travis.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+startAt=$(date)
+
+echo "Generating book"
+
+rm *.tmp *.aux
+latex book
+makeindex book
+latex book
+dvipdf book.dvi book.pdf
+
+echo Removed temporary files
+rm -f book.aux book.ind book.ilg book.log book.dvi book.idx book.toc book.haux book.hind book.image.tex book.tmp book.idv book.4tc book.lg book.xref
+
+echo "End (Start at: $startAt, End at: $(date))"
